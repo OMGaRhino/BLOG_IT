f3295b6d4be04745a222de8ed0b7ba67d7135c68
OMGaRhino <m_horne2009@hotmail.com>
2014-05-19 13:10:44 +1000

    client-side

---
+++ script.js
@@ -0,0 +1,33 @@
+var socket = io.connect();
+function addMessage(msg, pseudo) {
+	$("#chatEntries").append('<div class ="message"><p>' + pseudo + ' : ' + msg + '</p></div>');
+}
+
+function sentMessage() {
+	if ($('messageInput').val() != "")
+	{
+		socket.emit('message', $('#messageInput').val());
+		addMessage($('messageInput').val(), "Me", new Date().toISOString(), true);
+		$('#messageInput').val('');
+	}
+}
+
+function setPsuedo() {
+	if ($("#pseudoInput").val() != "")
+	{
+		socket.emit('setPsuedo', $("#pseudoInput").val());
+		$("#chatControls").show();
+		$("#pseudoInput").hide();
+		$("#pseudoSet").hide();
+	}
+}
+socket.on('message', function(data) {
+	addMessage(data['message'], data['pseudo']);
+});
+
+$(function() {
+	$("#chatControls").hide();
+	$("#pseudoSet").click(function() {setPsuedo()});
+	$("#submit").click(function() {sentMessage();});
+});
+
